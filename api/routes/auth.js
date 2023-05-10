1b909373abe5201243631260bc79259ced2a4b02
LouiseChili <chililove@users.noreply.github.com>
2023-05-10 17:08:02 +0200

    installing joi

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -2,9 +2,15 @@ const router = require('express').Router();
 const user = require('../models/user');
 
 router.post('/register', async (req, res) => {
+
+//return res.status(200).json({message: "Register route "});
+
 });
 
 router.post('/login', async (req, res) => {
+    
+    //return res.status(200).json({message: "Login route "});
+
 });
 
 module.exports = router;
