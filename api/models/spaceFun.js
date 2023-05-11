e0f26013bc04993d41bb2c42da4da71e8af27195
iNKidzy <nadq.nadia@gmail.com>
2023-05-11 14:06:37 +0200

    requiredTitle

--- api/models/spaceFun.js
+++ api/models/spaceFun.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 const Schema = mongoose.Schema;
 
 const SpaceFunSchema = new Schema({
-    name: String,
+    name: { type: String, required: true },
     project: [{type: Schema.Types.ObjectId, ref: 'Project'}]
 })
 
