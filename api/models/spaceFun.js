076b24635b05e6997657698dffe46f87b6bc586d
iNKidzy <nadq.nadia@gmail.com>
2023-05-11 13:54:10 +0200

    spaceFunCRUD

--- api/models/spaceFun.js
+++ api/models/spaceFun.js
@@ -1,19 +1,12 @@
 const mongoose = require("mongoose")
+
 const Schema = mongoose.Schema;
-// const ObjectId = require('mongoose').Types.ObjectId;
-// var BundleRef = mongoose.model('Bundle', Bundle);
 
 const SpaceFunSchema = new Schema({
     name: String,
     project: [{type: Schema.Types.ObjectId, ref: 'Project'}]
 })
 
-const SpaceFun = mongoose.model("SpaceFun", SpaceFunSchema)
+const spaceFun = mongoose.model("SpaceFun", SpaceFunSchema)
 
-//test data
-//  SpaceFun.create({
-//     name: "Space1",
-//     project: ['6458db96e8bd7ee58f9d7569'] // project MongoId
-//  })
-
-module.exports = SpaceFun
+module.exports = spaceFun
