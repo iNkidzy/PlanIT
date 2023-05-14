b31b4ec0572330088744bfab38edf7945ac1ea6b
LouiseChili <chililove@users.noreply.github.com>
2023-05-14 21:39:19 +0200

    error handling for projects

--- api/controllers/projectController.js
+++ api/controllers/projectController.js
@@ -1,61 +1,61 @@
 const project = require('../models/project')
 
-
-
     const createProject = async (req, res) => {
         try {
             data = req.body;
     
             const newProject = await project.create(data)
-            res.send(newProject)
+            res.status(200).send(newProject)
         } catch (err) {
-            console.log(err.message)
-            res.send(err.message)
+            res.status(500).send(err.message)
         }
     }
 
     const getAllProjects = async (req, res) => {
-        project.find()
-            .then(data => { res.send(data) })
-            .catch(err => {
-                res.status(500).send({ message: err.message })
-            })
-
+        try {
+            const getAllProjects = await project.find()
+            res.status(200).send(getAllProjects)
+        }catch(err) {
+            res.status(500).send({ message: err.message })
+        }
     }
 
     const getSpecificProject = async (req, res) => {
-        project.findById(req.params.id)
-            .then(data => { res.send(data) })
-            .catch(err => { res.status(500).send({ message: err.message }) })
-
+        try{
+            const findOneProject = await project.findById(req.params.id)
+            res.status(200).send(findOneProject)
+        } catch(err) {
+            res.status(500).send({ message: err.message })
     }
+}
 
     const updateProject = async (req, res) => {
         const id = req.params.id;
         const body = req.body;
-
-        project.findByIdAndUpdate(id, body)
-            .then(data => {
-                !data ? res.status(404).send({ message: "Cannot update project with id" + id + "Try Again" }) : res.send({ message: "Project successfully updated!" })
-
-            })
-            .catch(err => {
-                res.status(500).send({ message: "Error updating project with id:" + id })
-            })
-
+        try{
+            const updateProject = project.findByIdAndUpdate(id, body)
+            if(!updateProject) {
+                res.status(404).send({ message: "Cannot update project with id:" + id + ".Try Again!" })
+            } else {
+                res.status(200).send({ message: "Project successfully updated!" })
+            }
+        } catch (err){
+            res.status(500).send({ message: "Error updating project with id:" + id })
+        }
     }
 
     const deleteProject = async (req, res) => {
         const id = req.params.id;
 
-        project.findByIdAndDelete(id)
-            .then(data => {
-                res.send({ message: "Project successfully deleted!" })
-            })
-            .catch(err => {
-                res.status(500).send({ message: "Error deleting project with id:" + id })
-            })
-
+        try {
+            const deleteProject = await project.findByIdAndDelete(id)
+            if(!deleteProject) {
+            res.status(404).send({ message: "Error: Id not found" })
+        } else {
+            res.status(200).send({message: "Project successfully deleted!"})}
+        } catch(err) {
+            res.status(500).send({ message: err.message })
+        }
     }
 
     module.exports = { createProject, updateProject, getAllProjects, getSpecificProject, deleteProject }
