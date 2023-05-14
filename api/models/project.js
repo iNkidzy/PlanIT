90e824a44973ae9caa5152d33fb40db961a66570
iNKidzy <nadq.nadia@gmail.com>
2023-05-14 15:20:23 +0200

    Error Handling + refactoring

--- api/models/project.js
+++ api/models/project.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 const Schema = mongoose.Schema;
 
 const ProjectSchema = new Schema({
-    name: { type: String, required: true, unique: true },
+    name: { type: String, required: true},
     task: [{ type: Schema.Types.ObjectId, ref: 'Task' }],
     users: [{
         userId: { type: Schema.Types.ObjectId, ref: 'User' },
