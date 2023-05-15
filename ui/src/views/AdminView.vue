591926e5c8432eb32f3bb7de9676b208ddf86c59
LouiseChili <chililove@users.noreply.github.com>
2023-05-15 18:14:12 +0200

    created AdminView + AdminPageComponent + fetch and display users from api

---
+++ ui/src/views/AdminView.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+        <!-- <div v-for="user in state.users" key:="user.id">
+            <h4>
+{{ user.username }}
+            </h4>
+            <br>
+            <p>
+                {{ user.email }}
+            </p>
+        </div> -->
+
+
+        <v-table fixed-header height="300px">
+            <thead>
+                <tr>
+                    <th class="text-left">
+                        username
+                    </th>
+                    <th class="text-left">
+                        email
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="user in state.users" :key="user.id">
+                    <td>{{ user.username }}</td>
+                    <td>{{ user.email }}</td>
+                </tr>
+            </tbody>
+        </v-table>
+
+    </div>
+</template>
+
+<script>
+import { reactive } from 'vue';
+
+export default {
+    setup() {
+        const state = reactive({
+            users: {}
+        })
+
+        function getAllUsers() {
+            fetch("http://localhost:5500/api/user")
+                .then(res => res.json())
+                .then(data => {
+                    state.users = data
+                })
+        }
+        getAllUsers()
+        return { state, getAllUsers }
+    }
+
+}
+</script>
