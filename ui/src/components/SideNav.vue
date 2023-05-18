2508e709e67fd1a966776c00641be3496dedb9f0
iNKidzy <nadq.nadia@gmail.com>
2023-05-18 12:53:47 +0200

    spaceFun-crud

---
+++ ui/src/components/SideNav.vue
@@ -0,0 +1,39 @@
+<template>
+       <v-navigation-drawer
+        theme="dark"
+        rail
+        permanent
+      >
+        <v-list-item
+          nav
+          prepend-avatar="https://randomuser.me/api/portraits/women/75.jpg"
+        ></v-list-item>
+
+        <v-divider></v-divider>
+
+      </v-navigation-drawer>
+      <v-navigation-drawer permanent>
+        <v-list>
+          <v-list-item title="SpaceFun" value="SpaceFun"></v-list-item>
+
+          <v-list-item title="Projects" value="Projects"></v-list-item>
+        </v-list>
+        <template v-slot:append>
+          <div class="pa-2">
+            <v-btn block color="black">
+              Logout
+            </v-btn>
+          </div>
+        </template>
+      </v-navigation-drawer>
+</template>
+
+
+<script setup>
+
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
--- ui/src/router/index.js
+++ ui/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import SpaceFunDetailView from '../views/SpaceFunDetailView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,7 +17,21 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
-    }
+    },
+    {
+        path: '/spacefun',
+        name: 'spacefun',
+        component: () => import('../views/SpacefunView.vue')
+    },
+    {
+      path: '/spacefunDetail/:id',
+      name: 'spacefunDetail',
+      component: SpaceFunDetailView
+    },
+
+
+     // default redirect to home page
+   //  { path: '/:pathMatch(.*)*', redirect: '/' }
   ]
 })
 
