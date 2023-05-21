5d54c4429c4e1c2c115c09d7d95c0b8b12845aeb
iNKidzy <nadq.nadia@gmail.com>
2023-05-22 01:52:04 +0200

    sidenav-refac

--- ui/src/components/SideNav.vue
+++ ui/src/components/SideNav.vue
@@ -1,15 +1,13 @@
 <template>
   <v-navigation-drawer theme="dark" rail permanent>
-    <v-list-item nav prepend-avatar="https://randomuser.me/api/portraits/women/75.jpg"></v-list-item>
+    <v-list-item nav></v-list-item>
 
     <v-divider></v-divider>
 
   </v-navigation-drawer>
   <v-navigation-drawer permanent>
     <v-list>
-      <v-list-item title="SpaceFun" value="SpaceFun"></v-list-item>
-
-      <v-list-item title="Projects" value="Projects"></v-list-item>
+      <v-list-item @click="spacefunPage()" title="SpaceFun" value="SpaceFun"></v-list-item>
     </v-list>
     <template v-slot:append>
       <div class="pa-2">
@@ -24,10 +22,9 @@
 
 <script setup>
 import { inject } from 'vue';
-import { useRouter, useRoute } from 'vue-router'
+import { useRouter } from 'vue-router'
 
 const router = useRouter()
-const route = useRoute()
 
 const { setUser } = inject('authenticatedUser');
 
@@ -38,6 +35,9 @@ const logOut = async () => {
   router.push('/')
 }
 
+const spacefunPage = () => {
+    router.push('/spacefun')
+}
 
 </script>
 
