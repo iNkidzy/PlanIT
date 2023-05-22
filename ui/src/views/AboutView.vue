2562a97e3dde2224d0e8d5182242f8bd9b3b0976
iNKidzy <nadq.nadia@gmail.com>
2023-05-22 16:59:07 +0200

    welcomepage+aboutpage

---
+++ ui/src/views/AboutView.vue
@@ -0,0 +1,52 @@
+<template>
+    <v-container fluid>
+        <v-row class="justify-end">
+            <v-col cols="11">
+              <h2 class="btns text-h4 ps-4">Plan<span class="text-green">IT</span></h2>
+            </v-col>
+            <v-col cols="1" class="align-self-end">
+                <v-btn class="btns" variant="tonal" @click="back()" >Back</v-btn>
+            </v-col>
+        </v-row> 
+
+        <v-row class="justify-space-between">
+
+            <v-col cols="5">
+                <div style="margin-bottom:20%">
+                    <img src="../assets/nachilogo2.png" width="520" height="320"/>
+                
+                    <h5 class="text-white-smoke text-h5 ps-4"> 
+                        Created by this.developers  
+                        <br><br>
+                        It all started this week :D 
+                        <br><br>
+                        Here is our links to the book series of 
+                        <br>
+                        "How to slay in a day":
+                        <br><br>
+                        <a href="https://github.com/iNkidzy/">Book 1</a>
+                        <br><br>
+                        <a href="https://github.com/Chililove">Book 2</a>
+                    </h5>
+            </div>
+            </v-col>
+            <v-col cols="7" class="align-self-end">
+                <img src="../assets/nachi.png" width="900" height="820"/>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script setup>
+import { useRouter} from 'vue-router'
+
+const router = useRouter()
+const back = () => {router.push('/')}
+</script>
+
+<style scoped>
+.btns{
+  color: white; 
+}
+
+</style>
--- ui/src/views/WelcomeView.vue
+++ ui/src/views/WelcomeView.vue
@@ -1,23 +1,35 @@
 <template>
     <v-container fluid>
         <v-row class="justify-end">
-            <v-col cols="10">
+            <v-col cols="9">
               <h2 class="btns text-h4 ps-4">Plan<span class="text-green">IT</span></h2>
             </v-col>
-            <v-col cols="2" class="align-self-end">
+            <v-col cols="3" class="align-self-end">
+
+                <v-btn class="aboutbtn mr-6" variant="text" @click="about()">About</v-btn>
+
                 <v-btn class="btns mr-6" variant="text" @click="hideMe = true">Login</v-btn>
                 <v-btn class="btns" variant="tonal" @click="hideMeSignup = true">SignUp</v-btn>
             </v-col>
         </v-row> 
-        <v-row class="justify-center">
-            <v-col cols="12">
+        <v-row class="justify-space-between">
+            <v-col cols="7">
+                <div style="margin-top:10%">
+                    <h2 class="text-white text-h4 ps-4"> Start planning today</h2>
+                    <br>
+                    <h5 class="text-white-smoke text-h5 ps-6"> 
+                    PlanIT is a project managment tool <br>
+                    where you can plan and have fun in your SpaceFun
+                   
+                    </h5>
+                    <br><br>
+                    <img src="../assets/new.png" width="800" height="480"/> 
+                </div>             
+            </v-col>
+        <v-col cols="5" class="align-self-top">
                 <Login v-if="hideMe" @cancel="hideMe = false"></Login>
                 <Signup v-if="hideMeSignup" @cancel="hideMeSignup = false"></Signup>
-            </v-col>
-            <v-col cols="10">
-                <img src="../assets/frontpage.jpg"/>
-                <h2 class="text-white text-h4 ps-4"> Start planning today!</h2>
-            </v-col>
+        </v-col>
         </v-row>
     </v-container>
 </template>
@@ -27,18 +39,23 @@
 import Login from '../components/LoginComponents.vue'
 import Signup from '../components/SignupComponents.vue'
 import { ref } from 'vue'
+import { useRouter } from 'vue-router';
 
 const hideMe = ref(false)
 const hideMeSignup = ref(false)
-
+const router = useRouter()
+const about = () =>
+{
+    router.push('/about')
+}
 </script>
 
 <style scoped>
+.aboutbtn{
+    color: palevioletred
+}
 .btns{
   color: white; 
-}
-.color{
-    background-color: aqua;
 }
 
 </style>
